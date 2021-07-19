b51c9f9c2d97a5cf378adaf9ec52c69ba50fe793
Mahada Panji <mahada.panji@ymail.com>
2021-07-20 00:53:21 +0700

    first commit

---
+++ BackEndFile/WebApplication6/Migrations/202107180634051_hilangin-test-field.cs
@@ -0,0 +1,31 @@
+﻿namespace WebApplication6.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class hilangintestfield : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.SalesOrders",
+                c => new
+                    {
+                        SalesOrderId = c.Guid(nullable: false),
+                        NoBukti = c.String(),
+                        TglFaktur = c.DateTime(nullable: false),
+                        TglJthTempo = c.DateTime(nullable: false),
+                        SyaratBayar = c.String(),
+                        Disc = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        GrandTotal = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.SalesOrderId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.SalesOrders");
+        }
+    }
+}
