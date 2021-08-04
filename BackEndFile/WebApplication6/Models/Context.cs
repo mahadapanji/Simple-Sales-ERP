1d860e4d11f332a479d31e7e896bb631f10e1367
Mahada Panji <mahada.panji@ymail.com>
2021-08-04 21:36:35 +0700

    backendnya ke apus tambah lagi

---
+++ BackEndFile/WebApplication6/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication6.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base("Context")
+        {
+        }
+
+        public DbSet<SalesOrder> salesOrders { get; set; }
+
+    }
+}
---
+++ BackEndFile/WebApplication6/Models/SalesOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6.Models
+{
+    public class SalesOrder
+    {
+        public Guid SalesOrderId { get; set; }
+        public string NoBukti { get; set; }
+        public DateTime TglFaktur { get; set; }
+        public DateTime TglJthTempo { get; set; }
+        public string SyaratBayar { get; set; }
+        public decimal Disc { get; set; }
+        public decimal GrandTotal { get; set; }
+    }
+}
